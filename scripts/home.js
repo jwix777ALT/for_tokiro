dc1b973e4afd79544387f3a9cfc63bd6e59c61f8
jwix777 <69116290+jwix777ALT@users.noreply.github.com>
2022-11-08 21:05:02 +0300

    Some fixes

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,3 @@
-particlesJS.load('back', 'assets/particlesconfig.json', function() {
+particlesJS.load('back', 'https://raw.githubusercontent.com/jwix777ALT/for_tokiro/main/assets/particlesconfig.json', function() {
     console.log('callback - particles.js config loaded');
   });
